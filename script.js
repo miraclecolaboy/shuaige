546fcbbaca50c55f5058c4701119396621c073eb
miraclecolaboy <miraclecola4@gmail.com>
2025-11-17 23:27:22 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("注意！你将见到世界上最帅的人");
+// script.js
+
+// 可添加一些交互功能，例如卡片展开或其他功能
+document.querySelectorAll('.card a').forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        alert("还没想好干嘛 不要乱点呀~");
+    });
+});
